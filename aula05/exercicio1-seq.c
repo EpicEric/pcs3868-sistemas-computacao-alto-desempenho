c0953ce43337bc19db8bf4c3a698d91f0407d703
Mateus Nakajo <mateusnakajo@gmail.com>
2018-10-04 10:02:33 -0300

    Lint

--- aula05/exercicio1-seq.c
+++ aula05/exercicio1-seq.c
@@ -7,41 +7,41 @@
 
 double A[SIZE][SIZE];
 
-
 /*inicia matriz*/
 void init_matrix()
-  { 
-     int i,j;
-     for(i=0;i<SIZE;i++) {  
-       for (j=0;j<SIZE;j++){
-         A[i][j]=(3*i-j)/2.0;
-       }      
-     }
+{
+  int i, j;
+  for (i = 0; i < SIZE; i++)
+  {
+    for (j = 0; j < SIZE; j++)
+    {
+      A[i][j] = (3 * i - j) / 2.0;
+    }
   }
-
+}
 
 /*multiplicacao*/
 double sum_matrix()
+{
+  int i, j;
+  double sum = 0;
+  for (i = 0; i < SIZE; i++)
   {
-      int i,j;
-      double sum = 0;
-        for(i=0;i<SIZE;i++) {
-          for (j=0;j<SIZE;j++) 
-            sum = sum + A[i][j];
-      }
-      return sum;
+    for (j = 0; j < SIZE; j++)
+      sum = sum + A[i][j];
   }
+  return sum;
+}
 
-void main(argc, argv)
-int argc;
+void main(argc, argv) int argc;
 char **argv;
 {
   double sum;
   init_matrix();
   TIMER_CLEAR;
-  TIMER_START; 
+  TIMER_START;
   sum = sum_matrix();
   TIMER_STOP;
-  printf ("TEMPO [%d]: %12.7f\n",SIZE,TIMER_ELAPSED );
+  printf("TEMPO [%d]: %12.7f\n", SIZE, TIMER_ELAPSED);
   printf("SOMA: %lf\n", sum);
 }
