0723332a6bb4e37065ef5209d2200f0ab4eb5cfb
Mateus Nakajo <mateusnakajo@gmail.com>
2018-10-04 09:51:40 -0300

    Exercicio 1 da aula 5

---
+++ aula05/exercicio1-seq.c
@@ -0,0 +1,47 @@
+#include <stdio.h>
+#include <math.h>
+#include <sys/time.h>
+#include "mede_time.h"
+
+#define SIZE 1000
+
+double A[SIZE][SIZE];
+
+
+/*inicia matriz*/
+void init_matrix()
+  { 
+     int i,j;
+     for(i=0;i<SIZE;i++) {  
+       for (j=0;j<SIZE;j++){
+         A[i][j]=(3*i-j)/2.0;
+       }      
+     }
+  }
+
+
+/*multiplicacao*/
+double sum_matrix()
+  {
+      int i,j;
+      double sum = 0;
+        for(i=0;i<SIZE;i++) {
+          for (j=0;j<SIZE;j++) 
+            sum = sum + A[i][j];
+      }
+      return sum;
+  }
+
+void main(argc, argv)
+int argc;
+char **argv;
+{
+  double sum;
+  init_matrix();
+  TIMER_CLEAR;
+  TIMER_START; 
+  sum = sum_matrix();
+  TIMER_STOP;
+  printf ("TEMPO [%d]: %12.7f\n",SIZE,TIMER_ELAPSED );
+  printf("SOMA: %lf\n", sum);
+}
---
+++ aula05/exercicio1.c
@@ -0,0 +1,70 @@
+#include <stdio.h>
+#include <math.h>
+#include <sys/time.h>
+#include "mede_time.h"
+
+#define SIZE 1000
+
+double A[SIZE][SIZE];
+
+
+/*inicia matriz*/
+void init_matrix()
+  { 
+     int i,j;
+     #pragma omp parallel num_threads(2) shared(A) private(i,j)
+     {
+      #pragma omp for
+      for(i=0;i<SIZE;i++) {  
+       for (j=0;j<SIZE;j++){
+         A[i][j]=(3*i-j)/2.0;
+       }
+      }
+     }
+  }
+
+
+/*multiplicacao*/
+double sum_matrix()
+  {
+      int i,j;
+      double sum = 0;
+      #pragma omp parallel num_threads(2) shared (A) private(i,j) reduction (+:sum)
+      {
+        #pragma omp for    
+        for(i=0;i<SIZE;i++) {
+          for (j=0;j<SIZE;j++) 
+            sum = sum + A[i][j];
+        }
+      }
+      return sum;
+  }
+
+/*imprime matriz */
+void print_mat (A)
+double A[SIZE][SIZE];
+   {
+      int i,j;
+
+      for (i=0;i<10;i++){
+       for (j=0;j<10;j++)
+         printf (" %10.2f", A[i][j]);
+       puts ("");
+      }
+      puts ("");
+   }
+
+void main(argc, argv)
+int argc;
+char **argv;
+{
+  double sum;
+  init_matrix();
+  TIMER_CLEAR;
+  TIMER_START; 
+  sum = sum_matrix();
+  TIMER_STOP;
+  printf ("TEMPO [%d]: %12.7f\n",SIZE,TIMER_ELAPSED );
+  printf("SOMA: %lf\n", sum);
+}
+
