3c662a66536fc03895b126dbba27cdc974cd099f
Eric Pires <ericpires9@gmail.com>
2018-10-25 10:38:49 -0300

    Finish ex2 and change original ex1

--- aula08/exercicio1-original.c
+++ aula08/exercicio1-original.c
@@ -2,56 +2,55 @@
 #include <semaphore.h>
 #include <stdlib.h>
 #include <omp.h>
-sem_t sem1[5];
-sem_t sem2[5];
-int a[5];
-void inicia_a()
-{
-  int k;
-  for (k=0;k<5;k++)
-  {
-    a[k]=k+1;
-  }
-}
-void calcula(int i)
+#define think 20
+#define eat 10
+sem_t sem[5];
+
+void thinking(i)
+{ printf("filosofo %d thinking\n",i);
+  fflush(stdout);
+  usleep(think);
+} 
+void eating(i)
+{ printf("filosofo %d eating\n",i);
+  fflush(stdout);
+  usleep(eat);
+} 
+void filosofo(int i)
 { int k;
-  for(k=0;k<4;k++)
-  { 
-    sem_wait(&sem1[i]);
-    sem_wait(&sem2[i]);
-    printf("ANTES k=%d a[%d]=%d a[%d]=%d \n",k,i,a[i],(i+1)%5,a[(i+1)%5]);
-    fflush(stdout);
-    a[i]=a[i]+a[(i+1)%5];
-    a[(i+1)%5]=a[(i+1)%5]*2-a[i];
-    printf("DEPOIS k=%d a[%d]=%d a[%d]=%d \n",k,i,a[i],(i+1)%5,a[(i+1)%5]);
-    fflush(stdout);
-    sem_post(&sem1[i]);
-    sem_post(&sem2[i]);
+  for (k=0;k<100;k++) { 
+     thinking(i);
+     sem_wait(&sem[i]);
+     usleep(1);
+     sem_wait(&sem[(i+1)%5]);
+     eating(i);
+     sem_post(&sem[i]);
+     sem_post(&sem[(i+1)%5]);
   }
 }
 void main()
-{ int i;
+{
   printf("INICIO \n");
   fflush(stdout);
-  inicia_a();
-  for(i=0;i<5;i++) {
-    sem_init(&sem1[i],0,5);
-    sem_init(&sem2[i],0,5);
-  }
+  sem_init(&sem[0],0,1);
+  sem_init(&sem[1],0,1);
+  sem_init(&sem[2],0,1);
+  sem_init(&sem[3],0,1);
+  sem_init(&sem[4],0,1);
   #pragma omp parallel num_threads(5)
   {   
       #pragma omp sections
       {
          #pragma omp section
-         calcula(0);
+         filosofo(0);
          #pragma omp section
-         calcula(1);
+         filosofo(1);
          #pragma omp section
-         calcula(2);
+         filosofo(2);
          #pragma omp section
-         calcula(3);
+         filosofo(3);
          #pragma omp section
-         calcula(4);
+         filosofo(4);
       }
   }
       printf("FIM\n");
---
+++ aula08/exercicio2.c
@@ -0,0 +1,84 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <omp.h>
+
+#define SEQ_SIZE 12
+#define TRUE 0
+#define FALSE 1
+#define NTHREADS 4
+
+int main() {
+  int i_achou;
+  int i = 0;
+  int j;
+  int thread_num;
+  int igual;
+  char *buff;
+  char sequencia[SEQ_SIZE];
+  int file_size;
+  char **seq_vet;
+  int n_seq;
+  int k;
+  FILE *f = fopen("seq-teste.txt", "r");
+  if (f == NULL) {
+    printf("Erro na abertura do arquivo\n");
+    exit(1);
+  }
+  fseek(f, 0L, SEEK_END);
+  file_size = ftell(f);
+  fseek(f, 0L, SEEK_SET);
+  n_seq = (file_size + 1) / (SEQ_SIZE - 1);
+  seq_vet = (char **) malloc(n_seq * sizeof(char*));
+  for (k = 0; k < n_seq; k++) {
+    seq_vet[k] = malloc(SEQ_SIZE * sizeof(char));
+  }
+  while (!feof(f)) {
+    fgets(seq_vet[i], SEQ_SIZE, f);
+    /*printf("seq_vet[%d] =  %s\n ", i, seq_vet[i]);*/
+    i = i + 1;
+  }
+        
+  printf("file_size=%d\n", file_size);
+
+  printf("Digite a sequencia que deseja buscar: ");
+  scanf("%s", &sequencia);
+  igual = FALSE;
+
+  #pragma omp parallel num_threads(NTHREADS) shared(igual, i_achou)
+  {
+    int i_min, i_max;
+    int thread_num = omp_get_thread_num();
+    i_min = ((int) (n_seq * 1.0 / NTHREADS) * thread_num);
+    i_max = ((int) (n_seq * 1.0 / NTHREADS) * (thread_num + 1));
+    printf("t[%d], [%d:%d]\n", thread_num, i_min, i_max);
+    for (i = i_min; i < i_max; i++) {
+      if (igual == TRUE) {
+        break;
+      }
+      buff = seq_vet[i];
+      /*printf("i=%d buff %s\n ", i, buff);*/
+      for (j = 0; j < SEQ_SIZE - 2; j++) {
+        if (sequencia[j] != buff[j]) {
+          break;
+        }
+      }
+      if (j == SEQ_SIZE - 2) {
+        #pragma omp critical
+        {
+          igual = TRUE;
+          i_achou = i;
+        }
+        break;
+      }
+    }
+  }
+
+  if (igual == TRUE) {
+    printf("Sequencia encontrada na linha %d\n", i_achou);
+  } else {
+    printf("Sequencia nao encontrada\n");
+  }
+
+  fclose(f);
+  return 0;
+}
